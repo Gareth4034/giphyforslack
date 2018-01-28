746b104d05731585ed89624458cdd17ea06b3b26
Gareth Lancaster <gareth40342@outlook.com>
2018-01-28 20:48:39 +0000

    Initial test

---
+++ src/main/java/GiphyAccess.java
@@ -0,0 +1,5 @@
+package lancaster.apps.giphyForSlack;
+
+public class GiphyAccess {
+
+}
---
+++ src/main/java/GiphyForSlackConfiguration.java
@@ -0,0 +1,5 @@
+import io.dropwizard.Configuration;
+
+public class GiphyForSlackConfiguration extends Configuration {
+
+}
